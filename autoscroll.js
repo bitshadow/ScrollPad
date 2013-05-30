fce09ef5835039759532e1571fecde3868bc2805
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2013-05-30 20:26:40 +0530

    click and drag added

--- autoscroll.js
+++ autoscroll.js
@@ -1,143 +1,152 @@
-function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
 
-  if (typeof stroke == "undefined" ) {
-    stroke = true;
+var scrollPad = {
+
+  curx: 0,
+
+  cury: 0,
+
+  roundRect: function(ctx, x, y, width, height, radius, fill, stroke) {
+
+      if (typeof stroke == "undefined" ) {
+          stroke = true;
+      }
+
+      if (typeof radius === "undefined") {
+          radius = 5;
+      }
+
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+      ctx.lineTo(x + width, y + height - radius);
+      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+      ctx.lineTo(x + radius, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+      ctx.lineTo(x, y + radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+
+      if (stroke) {
+          ctx.stroke();
+      }
+
+      if (fill) {
+          ctx.fill();
+      }
+  },
+
+  clientSize: function() {
+
+      var size = {
+
+          width: window.innerWidth || document.body.clientWidth,
+          height: window.innerHeight || document.body.clientHeight
+      }
+
+      return size;
+  },
+
+  bodySize: function() {
+
+      var w = window.getComputedStyle(document.body,null).getPropertyValue("width");
+      var h = window.getComputedStyle(document.body,null).getPropertyValue("height")
+
+      var size = {
+
+          width: parseInt(w),
+          height: parseInt(h)
+      }
+
+      return size;
+  },
+
+  getViewSize: function() {
+
+      var clsize = this.clientSize();
+      var bsize = this.bodySize();
+      var w = (clsize.width * 75) / parseInt(bsize.width);
+      var h = (clsize.height * 75) / parseInt(bsize.height);
+
+      var size = {
+
+          width: parseInt(w),
+          height: parseInt(h)
+      }
+
+      return size;
+  },
+
+  draw: function(canvas, x, y) {
+      var context = canvas.getContext('2d');
+      var vsize = this.getViewSize();
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.fillStyle = "white";
+      this.roundRect(context,x-(vsize.width/2), y-(vsize.height/2), vsize.width, vsize.height, 2, true, false);
+  },
+
+  //position in canvas
+  scrollPage: function(c, pos) {
+      var message="";
+      var v = this.getViewSize();
+      var b = this.bodySize();
+
+      var scroll = {
+          x: b.width / (c.width-v.width),
+          y: b.height / (c.height-v.height)
+      };
+
+      $('html,body').animate({ scrollTop: ((pos.y - (v.height / 2)) * scroll.y) }, 0);
+      $('html,body').animate({ scrollLeft: ((pos.x - (v.width  / 2)) * scroll.x) }, 0);
+  },
+
+  scrollpad: function(canvas, pos) {
+      var vsize = this.getViewSize();
+      var x = pos.x;
+      var y = pos.y;
+
+      if ((pos.x - (vsize.width/2)) <= 0 ) {
+          x = vsize.width/2;
+      }
+
+      if ((pos.x + (vsize.width/2)) >= canvas.width) {
+          x = canvas.width - vsize.width/2;
+      }
+
+      if ((pos.y - (vsize.height/2)) <= 0 ) {
+          y = vsize.height/2;
+      }
+
+      if ((pos.y + (vsize.height/2)) >= canvas.height) {
+          y = canvas.height - vsize.height/2;
+      }
+
+      this.draw(canvas, x, y);
+      this.scrollPage(canvas, pos);
+  },
+
+  getMousePos: function(canvas, evt) {
+
+      var rect = canvas.getBoundingClientRect();
+
+      return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+      };
   }
-
-  if (typeof radius === "undefined") {
-    radius = 5;
-  }
-
-  ctx.beginPath();
-  ctx.moveTo(x + radius, y);
-  ctx.lineTo(x + width - radius, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-  ctx.lineTo(x + width, y + height - radius);
-  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-  ctx.lineTo(x + radius, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-  ctx.lineTo(x, y + radius);
-  ctx.quadraticCurveTo(x, y, x + radius, y);
-  ctx.closePath();
-
-  if (stroke) {
-    ctx.stroke();
-  }
-
-  if (fill) {
-    ctx.fill();
-  }
-}
-
-function clientSize() {
-
-    var size = {
-
-        width: window.innerWidth || document.body.clientWidth,
-        height: window.innerHeight || document.body.clientHeight
-    }
-
-    return size;
-}
-
-function bodySize() {
-
-    var w = window.getComputedStyle(document.body,null).getPropertyValue("width");
-    var h = window.getComputedStyle(document.body,null).getPropertyValue("height")
-
-    var size = {
-
-        width: parseInt(w),
-        height: parseInt(h)
-    }
-
-    return size;
-}
-
-function getViewSize() {
-
-  var clsize = clientSize();
-  var bsize = bodySize();
-  var w = (clsize.width * 100) / parseInt(bsize.width);
-  var h = (clsize.height * 100) / parseInt(bsize.height);
-
-  var size = {
-
-    width: parseInt(w),
-    height: parseInt(h)
-
-  }
-
-  return size;
-}
-
-function draw(canvas, x, y) {
-    var context = canvas.getContext('2d');
-    var vsize = getViewSize();
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "white";
-    roundRect(context,x-(vsize.width/2), y-(vsize.height/2), vsize.width, vsize.height, 2, true, false);
-}
-
-//position in canvas
-function scrollPage(c, pos) {
-    var message="";
-    var v = getViewSize();
-    var b = bodySize();
-
-    var scroll = {
-        x: b.width / (c.width-v.width),
-        y: b.height / (c.height-v.height)
-    };
-
-    $('html,body').animate({ scrollTop: ((pos.y - (v.height / 2)) * scroll.y) }, 0);
-    $('html,body').animate({ scrollLeft: ((pos.x - (v.width  / 2)) * scroll.x) }, 0);
-}
-
-function scrollpad(canvas, pos) {
-    var vsize = getViewSize();
-    var x = pos.x;
-    var y = pos.y;
-
-    if ((pos.x - (vsize.width/2)) <= 0 ) {
-        x = vsize.width/2;
-    }
-
-    if ((pos.x + (vsize.width/2)) >= canvas.width) {
-        x = canvas.width - vsize.width/2;
-    }
-
-    if ((pos.y - (vsize.height/2)) <= 0 ) {
-        y = vsize.height/2;
-    }
-
-    if ((pos.y + (vsize.height/2)) >= canvas.height) {
-        y = canvas.height - vsize.height/2;
-    }
-
-    draw(canvas, x, y);
-    scrollPage(canvas, pos);
-}
-
-function getMousePos(canvas, evt) {
-
-    var rect = canvas.getBoundingClientRect();
-
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
 }
 
 $(document).ready(function() {
     var canvas = document.getElementById('scrollpad');
+    var mousemove = 0;
+    var mousedown = 0
 
     canvas.addEventListener('mousemove', function(evt) {
 
-        var pos = getMousePos(canvas, evt);
+        var pos = scrollPad.getMousePos(canvas, evt);
 
-        scrollpad(canvas, pos);
+        scrollPad.scrollpad(canvas, pos);
 
         //var message = 'Mouse position: ' + pos.x + ',' + pos.y;
 
@@ -145,9 +154,27 @@ $(document).ready(function() {
 
     canvas.addEventListener('click', function(evt) {
 
-        var pos = getMousePos(canvas, evt);
+        var pos = scrollPad.getMousePos(canvas, evt);
 
     }, false);
+
+
+    canvas.addEventListener("mousedown", function(){
+        mousedown = 1;
+    }, false);
+
+    canvas.addEventListener("mousemove", function(){
+        if (mousedown == 1) {
+            mousemove = 1;
+        }
+    }, false);
+
+    canvas.addEventListener("mouseup", function(){
+        mousedown = 0;
+        if(mousedown == 0 && mousemove == 0 )
+            console.log("click");
+    }, false);
+
 });
 
 
